14dddc686664fc08bcdd68211f5119804539ae74
Esa Mäkipää <esa.makipaa@gmail.com>
2020-08-04 12:10:00 +0300

    Exercises 3.1 - 3.6 done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// console.log('Hello World and puhelinluettelon backend')
-//const http = require('http')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let persons = [
   {
@@ -36,7 +36,68 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
+
+// get phonebook info
+app.get('/info', (req, res) => {
+    date = new Date()
+    res.send(`
+      <div>
+        <p>Phonebook has info for ${persons.length} people </p>
+        <p> ${date}</p>
+      </div>
+    `)
+  })
+
+// get person with specific id
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+// delete data with specific id
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+// add new data to phonebook
+app.post('/api/persons/', (req, res) => {
+
+  const body = req.body
   
+  // check name and number
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name or number or both missing' 
+    })
+  }
+  
+  // check if person already exists
+  if (persons.map(person => person.name).includes(body.name)) { 
+    return res.status(400).json({ 
+      error: `${body.name} is already added to phonebook, name must be unique`
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000) + 1,
+  }
+  
+  persons = persons.concat(person)
+  
+  res.json(person)
+})
+
 const port = 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
